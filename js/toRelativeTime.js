b822bcd20d948386095e8bc9d31ca3fb3b4f21a2
Luke Smith <lsmith@yahoo-inc.com>
2009-03-08 20:57:17 -0700

    initial drop

---
+++ js/toRelativeTime.js
@@ -0,0 +1,16 @@
+YUI.add('relativetime', function (Y) {
+    Y.toRelativeTime = function (d,from) {
+        d = d || new Date();
+        from = from || new Date();
+
+        var delta = (from.getTime() - d.getTime()) / 1000;
+
+        return delta < 60 ? "less than a minute ago" :
+               delta < 120 ? "about a minute ago" :
+               delta < 3600 ? Math.floor(delta/60) + " minutes ago" :
+               delta < 7200 ? "about an hour ago" :
+               delta < 86400 ? "about " +Math.floor(delta/3600)+ " hours ago" :
+               delta < 172800 ? "1 day ago" :
+               Math.floor(delta/86400)+ " days ago";
+    };
+},'@VERSION@');
